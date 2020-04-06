348ec3fda4311ce13b1cafa460726d8fcb7cc359
Gaetan Chambouvet <gct2@3ds.com>
2020-04-06 14:15:36 +0200

    Code cleanup

--- extension.js
+++ extension.js
@@ -1,14 +1,10 @@
 const EventsWatcher = require("./src/EventsWatcher");
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
     new EventsWatcher(context).run();
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
