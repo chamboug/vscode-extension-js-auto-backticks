591bbf8f29e672e8aa144e509e6a2ee67056dd97
Gaetan Chambouvet <gct2@3ds.com>
2022-05-04 14:32:14 +0200

    Improve quotes detection system

--- src/EventsWatcher.js
+++ src/EventsWatcher.js
@@ -1,4 +1,5 @@
 const vscode = require("vscode");
+const { tsquery } = require("@phenomnomnominal/tsquery");
 
 module.exports = class EventsWatcher {
     constructor(context) {
@@ -6,45 +7,88 @@ module.exports = class EventsWatcher {
     }
     run = () => {
         vscode.workspace.onDidChangeTextDocument(
-            event => {
+            (event) => {
                 if (!this.shouldCheckForBackticks(event.document, event.contentChanges)) {
                     return;
                 }
 
-                const lineIndex = event.contentChanges[0].range.start.line;
+                const changeStart = event.contentChanges[0].range.start;
+                const lineIndex = changeStart.line;
                 const line = event.document.lineAt(lineIndex);
-                const lineContent = line.text;
-                const regex = /("|')(?<beforeBrace>.*)\$\{(?<afterBrace>.*)("|')/g;
-                const regexResult = regex.exec(lineContent);
-                if (!regexResult) {
+
+                if (line.text.charAt(changeStart.character - 1) !== "$") {
                     return;
                 }
 
-                const replaceRange = new vscode.Range(
-                    new vscode.Position(lineIndex, regexResult.index),
-                    new vscode.Position(lineIndex, regex.lastIndex)
-                );
+                try {
+                    const ast = tsquery.ast(line.text);
+                    const wrappingString = tsquery(
+                        ast,
+                        `StringLiteral[pos<${changeStart.character}][end>${changeStart.character}]`
+                    )?.[0];
 
-                const editor = vscode.window.activeTextEditor;
-                if (!editor) {
-                    return;
+                    if (!wrappingString) {
+                        return;
+                    }
+
+                    /**
+                     * In the following examples, wrappingString.pos === 9
+                     * const a ="${"
+                     * const a = "${"
+                     * const a =    "${"
+                     *
+                     * As spaces are not taken into account, we need to locate the first quote.
+                     */
+                    let firstQuotePos = wrappingString.pos;
+                    while (['"', "'"].every((char) => line.text.charAt(firstQuotePos) !== char)) {
+                        firstQuotePos++;
+                    }
+
+                    const firstQuoteRange = new vscode.Range(
+                        new vscode.Position(lineIndex, firstQuotePos),
+                        new vscode.Position(lineIndex, firstQuotePos + 1)
+                    );
+                    const lastQuoteRange = new vscode.Range(
+                        new vscode.Position(lineIndex, wrappingString.end - 1),
+                        new vscode.Position(lineIndex, wrappingString.end)
+                    );
+
+                    const editor = vscode.window.activeTextEditor;
+                    if (!editor) {
+                        return;
+                    }
+
+                    editor
+                        .edit((editBuilder) => {
+                            [firstQuoteRange, lastQuoteRange].forEach((range) => {
+                                editBuilder.replace(range, "`");
+                            });
+                            editBuilder.insert(new vscode.Position(lineIndex, changeStart.character + 1), "}");
+                        })
+                        .then(() => {
+                            const newPosition = new vscode.Position(changeStart.line, changeStart.character + 1);
+                            const newSelection = new vscode.Selection(newPosition, newPosition);
+                            editor.selection = newSelection;
+                        });
+                } catch {
+                    // silent fail
                 }
-                editor.edit(editBuilder => {
-                    editBuilder.replace(replaceRange, `\`${regexResult.groups.beforeBrace}\${${regexResult.groups.afterBrace}}\``);
-                });
             },
             null,
             this.context.subscriptions
         );
-    }
+    };
     shouldCheckForBackticks(document, contentChanges) {
-        if (!document.isDirty || document.isClosed) { // Only work with open and dirty editors
+        if (!document.isDirty || document.isClosed) {
+            // Only work with open and dirty editors
             return false;
         }
-        if (contentChanges.length === 0) { // If no changes, then return
+        if (contentChanges.length === 0) {
+            // If no changes, then return
             return false;
         }
-        if (contentChanges[0].text !== "{") { // Only the { char will trigger verification
+        if (contentChanges[0].text !== "{") {
+            // Only the { char will trigger verification
             return false;
         }
         return true;
